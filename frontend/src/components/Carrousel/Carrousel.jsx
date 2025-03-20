8c39f0ddef6ca3bbc8ab2d3e60efe99e2e5b05ea
Vaporz <tupeuxpasteco@protonmail.com>
2025-03-20 22:43:09 +0100

    Ajout du composant Carrousel pour afficher les images des logements, mise à jour de la page Logements pour intégrer le Carrousel, et refactorisation des styles associés.

---
+++ frontend/src/components/Carrousel/Carrousel.jsx
@@ -0,0 +1,33 @@
+import './Carrousel.scss';
+import React from 'react';
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import Chevron from '../../components/Chevron/Chevron';
+
+export default function Carrousel({ pictures }) {
+    const [ ImgIndex, setImgIndex ] = useState(0);
+    const handleNext = () => {
+        setImgIndex((ImgIndex + 1) % pictures.length);
+    }
+    const handlePrev = () => {
+        setImgIndex((ImgIndex - 1 + pictures.length) % pictures.length);
+    }
+
+    return (
+        <div className="carrousel">
+            {pictures.map((picture, index) => (
+                <img key={index} className={`carrousel__img ${index === ImgIndex ? 'carrousel__img--active' : ''}`} src={picture} alt="" />
+            ))}
+            <button className="carrousel__btn carrousel__btn--prev" onClick={handlePrev} aria-label="Image précédente">
+                <Chevron direction="left" />
+            </button>
+            <button className="carrousel__btn carrousel__btn--next" onClick={handleNext} aria-label="Image suivante">
+                <Chevron direction="right" />
+            </button>
+        </div>
+    )
+}
+
+Carrousel.propTypes = {
+    pictures: PropTypes.arrayOf(PropTypes.string).isRequired,
+}
--- frontend/src/pages/Logements/Logements.jsx
+++ frontend/src/pages/Logements/Logements.jsx
@@ -1,13 +1,25 @@
 import { useParams } from "react-router";
 import logementsData from "../../data/logements.json";
+import Carrousel from "../../components/Carrousel/Carrousel";
 
 export default function Logements() {
   let params = useParams();
   console.log(logementsData);
   let logement = logementsData.find((logement) => logement.id === params.id);
+
   return (
     <div className="container">
-      <div>{logement.title}</div>
+      <Carrousel pictures={logement.pictures} />
+      <section className="logements__container">
+        <header className="logements__header">
+          <h1 className="logements__title">{logement.title}</h1>
+          <p className="logement__location">{logement.location}</p>
+        </header>
+        <div className="logements__infos">
+        </div>
+        <div className="logements__details">
+        </div>
+      </section>
     </div>
   );
 }
