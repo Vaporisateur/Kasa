aa44ac8d2626bf31fd803e632df2188241424c44
Vaporz <tupeuxpasteco@protonmail.com>
2025-04-09 16:29:35 +0200

    Refonte des styles du footer pour améliorer la mise en page, mise à jour des étiquettes NavData pour plus de cohérence, amélioration de la structure et des styles de la page NotFound pour une meilleure expérience utilisateur.

--- frontend/src/pages/NotFound/NotFound.jsx
+++ frontend/src/pages/NotFound/NotFound.jsx
@@ -1,4 +1,12 @@
+import './NotFound.scss';
+
 export default function NotFound() {
-    return <h1>Erreur 404 - Page non trouvée</h1>;
+    return (
+      <main className="notfound container">
+        <h1 className="notfound__title">404</h1>
+        <span className="notfound__subtitle">Oups! La page que vous demandez n'existe pas.</span>
+        <a href="/" className="notfound__link">Retourner sur la page d'accueil</a>
+      </main>
+    );
   }
   
